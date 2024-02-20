d2603b3b0ac5b1c12cf2a1cbdfe9734a3f765477
xinobi-vm <rkarthik.vmc@gmail.com>
2024-02-19 19:12:17 -0500

    Added Authorization Server Details

--- functions/api.js
+++ functions/api.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const { expressjwt: jwt } = require("express-jwt");
+const jwks = require("jwks-rsa");
 
 const app = express();
 const router = express.Router();
+
+const jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    ratelimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri:
+      "https://dev-20808697.okta.com/oauth2/ausf8unzmxmFXB14X5d7/v1/keys",
+  }),
+  audience: "api://aclabs-backend-api.netlify.app",
+  issuer: "https://dev-20808697.okta.com/oauth2/ausf8unzmxmFXB14X5d7",
+  algorithms: ["RS256"],
+});
 
 router.get("/", (req, res) => {
   res.json({
@@ -19,6 +34,27 @@ router.get("/org", (req, res) => {
       type: "public",
     },
   });
+});
+
+router.get("/org/private", jwtCheck, (req, res) => {
+  res.json({
+    org: "AC Labs - BackEnd Resource Server",
+    version: "1.0",
+    author: "KR",
+    api: {
+      type: "private",
+    },
+  });
+});
+
+router.use(function (err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({
+      status: "Invalid Token",
+    });
+  } else {
+    next(err);
+  }
 });
 
 app.use("/", router);
